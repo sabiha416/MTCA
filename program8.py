7716904a9facfef3d51f1e9740bae75ee5b8018c
SABIHA SHAIK <121341886+sabiha416@users.noreply.github.com>
2022-12-31 16:48:36 +0530

    Add files via upload

--- program8.py
+++ program8.py
@@ -1,16 +1,9 @@
-##x1=5
-##y1=5
-##x2='Hello'
-##y2='Hello'
-##x3=[1,2,3]
-##y3=[1,2,3]
-##print(x1 is not y1)
-##
-##print(x2 is y2)
-##
-##print(x3 is y3)
+import sqlite3 as lite
+con=lite.connect('mtica.db')
 
 
-print(True and True)
-print(True and False)
-print( not True)
+cur=con.cursor()
+cur.execute("SELECT * FROM cars")
+rows=cur.fetchall()
+for row in rows:
+    print(row)
